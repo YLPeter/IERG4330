edcbe80b419b83e582de5b64da5e64834b81a77d
YLPeter <q510939123@gmail.com>
2021-03-29 22:52:44 +0800

    Update q3a_1.py

--- q3a_1.py
+++ q3a_1.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         (url,(_,rank)) = input
         return links.map(lambda dest: (dest,rank/links.size))
     contrib = links.join(ranks).flatMap(case_map)
-    contrib = spark.sparkContext.parallelize(contrib.collect())
+    contrib = contrib.map(lambda x: x)
     for i in contrib:
         print("out3 ",contrib)
     ranks = contrib.reduceByKey(lambda a, b: a + b).mapValues(lambda x: 0.15+0.85 * x)
