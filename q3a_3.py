d46adcfc2f04d1cbea07c5fc53320241f763a091
YLPeter <q510939123@gmail.com>
2021-03-29 22:53:34 +0800

    Create q3a_3.py

---
+++ q3a_3.py
@@ -0,0 +1,55 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import print_function
+
+import sys
+from operator import add
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: RageRank <file>", file=sys.stderr)
+        sys.exit(-1)
+   
+        
+        
+    spark = SparkSession\
+        .builder\
+        .appName("PythonRageRank")\
+        .getOrCreate()
+
+    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
+    data = lines.filter(lambda x: x.encode("ascii", "ignore")[0]!='#')
+    links = data.map(lambda x: x.split('\t'))
+    keys = links.groupByKey()
+    ranks = keys.map(lambda x: (x[0],1))
+    def case_map(input):
+        (url,(_,rank)) = input
+        return links.map(lambda dest: (dest,rank/links.size))
+    contrib = links.join(ranks).flatMap(case_map)
+    contrib = contrib.map(lambda x: x)
+    for i in contrib:
+        print("out3 ",contrib)
+    ranks = contrib.reduceByKey(lambda a, b: a + b).mapValues(lambda x: 0.15+0.85 * x)
+    output2 = ranks.collect()
+    for i in output2:
+        print("out2 ",i)
+
+    spark.stop()
