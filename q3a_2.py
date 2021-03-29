6e87efce30261386541af50fe3cf2829d760427b
YLPeter <q510939123@gmail.com>
2021-03-29 22:28:32 +0800

    Update q3a_2.py

--- q3a_2.py
+++ q3a_2.py
@@ -39,12 +39,11 @@ if __name__ == "__main__":
     data = lines.filter(lambda x: x.encode("ascii", "ignore")[0]!='#')
     links = data.map(lambda x: x.split('\t'))
     keys = links.groupByKey()
-    ranks = keys.map(lambda x: (x[0],1,len(x[1])))
+    ranks = keys.map(lambda x: (x[0],1))
     def case_map(input):
-        url,link_rank = input
-        rank = link_rank[1]
-        return links.map(lambda dest: (dest,rank/len(links)))
-    contrib = links.join(ranks)
+        (url,(_,rank)) = input
+        return links.map(lambda dest: (dest,rank/links.size))
+    contrib = links.join(ranks).flatMap(case_map)
     
     output2 = contrib.collect()
     for i in output2:
