df3043d5c48304b3560d2071c0ca1ea50d0c5bfc
YLPeter <q510939123@gmail.com>
2021-05-12 00:25:01 +0800

    Update hw5_1a.py

--- hw5/hw5_1a.py
+++ hw5/hw5_1a.py
@@ -37,8 +37,8 @@ if __name__ == "__main__":
     print("ans3_total:",ans3_total)
     ans4_total = df_mooc.count()
     print("ans4_total:",ans4_total)
-    ans5_total = df_mooc.groupBy("TARGETID").count().show()
-    ans6_total = df_mooc.groupBy("USERID").count().show()
+    ans5_total = df_mooc.groupBy("TARGETID").count().sort(desc("count")).show()
+    ans6_total = df_mooc.groupBy("USERID").count().sort(desc("count")).show()
     #filtedDF = df_mooc.select(df_mooc['USERID'], df_mooc['TARGETID'], df_mooc['TIMESTAMP']).show()
     
     #df = spark.read.csv(sys.argv[1], sep=r'\t', header=True)
