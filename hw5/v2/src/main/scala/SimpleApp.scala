9b9ff6540198be973d90aa767739c9651662c6c3
YLPeter <q510939123@gmail.com>
2021-05-12 23:26:27 +0800

    v2

--- hw5/v2/src/main/scala/SimpleApp.scala
+++ hw5/v2/src/main/scala/SimpleApp.scala
@@ -15,8 +15,8 @@ object SimpleApp {
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
     val logData = spark.read.textFile(logFile).cache()
 	
-	val dag = GraphLoader.edgeListFile(spark.sparkContext, "/opt/spark/workplace/dag_edge_list.csv")
-    val edges = GraphLoader.edgeListFile(spark.sparkContext, "/opt/spark/workplace/edge_list.csv")
+	val dag = GraphLoader.edgeListFile(spark.sparkContext, "/opt/spark/workplace/dag_edge_list.txt")
+    val edges = GraphLoader.edgeListFile(spark.sparkContext, "/opt/spark/workplace/edge_list.txt")
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println(s"Lines with a: $numAs, Lines with b: $numBs")
